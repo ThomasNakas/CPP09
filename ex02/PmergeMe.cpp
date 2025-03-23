f3ce369963e7efb9b372b229f3829fafcd1e53c6
Thomas Nakas <tnakas@3-E-3.42heilbronn.de>
2025-03-23 19:15:36 +0100

    setting up the ex02 and writing the OCF functions

---
+++ ex02/PmergeMe.cpp
@@ -0,0 +1,18 @@
+#include "PmergeMe.hpp"
+
+PmergeMe::PmergeMe()
+{
+
+};
+PmergeMe::~PmergeMe()
+{
+};
+PmergeMe::PmergeMe(const PmergeMe& other)
+{
+	*this = other;
+};
+PmergeMe& PmergeMe::operator=(const PmergeMe& other)
+{
+	(void)other;
+	return *this;
+};
---
+++ ex02/PmergeMe.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <deque>
+#include <chrono>
+#include <algorithm>
+class PmergeMe
+{
+	public:
+		PmergeMe();
+		~PmergeMe();
+		PmergeMe(const PmergeMe& other);
+		PmergeMe& operator=(const PmergeMe& other);
+
+};
